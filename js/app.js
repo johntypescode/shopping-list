800e96019744d67da97a01e6993147349a383ef0
John <186672498+johntypescode@users.noreply.github.com>
2024-12-06 09:57:59 -0500

    Functionality to add new item to the list when the Add Item button is clicked... initial test.

--- js/app.js
+++ js/app.js
@@ -6,6 +6,19 @@ window.onload = () => {
 	const addItemBttn = $.querySelector('button#append-list-item');
 	const shoppingList = $.querySelector('ul#shopping-list');
 
+	addItemBttn.addEventListener('click', (e) => {
+		e.preventDefault(); // Prevent default behavior of button click event
 
+		const newItemValue = newItemInput.value;
+
+		if(newItemValue.length >= 4) {
+			const newListItem = $.createElement('li');
+			newListItem.textContent = newItemValue;
+			shoppingList.appendChild(newListItem);
+		} else {
+			alert('Error: item should container at least four (4) characters; try again.');
+			return;
+		}
+	});
 
 };
