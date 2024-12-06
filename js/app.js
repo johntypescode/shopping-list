2da4c5e5e2fe0844c128cbd4a21b4fc8846fd1a2
John <186672498+johntypescode@users.noreply.github.com>
2024-12-06 09:51:40 -0500

    Form element accessor variables

---
+++ js/app.js
@@ -0,0 +1,11 @@
+window.onload = () => {
+
+
+	const $ = document;
+	const newItemInput = $.querySelector('input#new-list-item');
+	const addItemBttn = $.querySelector('button#append-list-item');
+	const shoppingList = $.querySelector('ul#shopping-list');
+
+
+
+};
